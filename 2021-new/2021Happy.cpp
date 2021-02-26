a8e1aa8f428aa50d8562a9a8e3d8df8f285cd931
JERRY-Z-J-R <www.zjr1846334075@foxmail.com>
2021-02-26 11:55:09 +0800

    reset

--- 2021-new/2021Happy.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	cout << "Happy new Year" << endl;
-
-	return 0;
-}
--- school-curriculum/project01/1-2/1-2-2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using std::cout;
-
-int main()
-{
-    cout << "cout" << std::endl;
-
-    return 0;
-}
